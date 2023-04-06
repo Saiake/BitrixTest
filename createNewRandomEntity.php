b395faa5b547483e3bc462c551dac7342e89fca0
Saiake <65845952+Saiake@users.noreply.github.com>
2023-04-06 11:26:29 +0500

    remove uncorrent fields adding

--- createNewRandomEntity.php
+++ createNewRandomEntity.php
@@ -5,18 +5,12 @@ $randomTypeId = rand(2, 4);
 
 switch ($randomTypeId) {
     case 2:
-        CRest::call('crm.deal.add', [
-            'TITLE' => 'buttonMake'
-        ]);
+        CRest::call('crm.deal.add', []);
         break;
     case 3:
-        CRest::call('crm.contact.add', [
-            'NAME' => 'buttonMake'
-        ]);
+        CRest::call('crm.contact.add', []);
         break;
     case 4:
-        CRest::call('crm.company.add', [
-            'TITLE' => 'buttonMake'
-        ]);
+        CRest::call('crm.company.add', []);
         break;
 }
